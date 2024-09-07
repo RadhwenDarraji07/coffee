a851785936bf07a05239e2500b44f2ca22e7c211
RadhwenDarraji07 <rathwenndarraji@gmail.com>
2024-09-07 14:58:07 -0700

    dealing with css++

--- coffeeshop/frontend/coffee-shop-app/src/components/Prods.js
+++ coffeeshop/frontend/coffee-shop-app/src/components/Prods.js
@@ -21,62 +21,68 @@ const products = [
       color: 'Dark Roast',
     },
     {
-        id: 1,
-        name: 'Coorg Pure Arabica Coffee',
-        href: '#',
-        imageSrc: 'https://hattikaapi.com/wp-content/uploads/2019/08/Pure-Pouch-1.png',
-        imageAlt: 'Coorg Pure Arabica Coffee',
-        price: '$15',
-        color: 'Dark Roast',
-      }, {
-        id: 1,
-        name: 'Coorg Pure Arabica Coffee',
-        href: '#',
-        imageSrc: 'https://hattikaapi.com/wp-content/uploads/2019/08/Chicory-Tin.png',
-        imageAlt: 'Coorg Pure Arabica Coffee',
-        price: '$15',
-        color: 'Dark Roast',
-      }, {
-        id: 1,
-        name: 'Coorg Pure Arabica Coffee',
-        href: '#',
-        imageSrc: 'https://www.gestures.in/cdn/shop/products/890614350059-1_1200x1200.jpg?v=1652438506',
-        imageAlt: 'Coorg Pure Arabica Coffee',
-        price: '$15',
-        color: 'Dark Roast',
-      }, {
-        id: 1,
-        name: 'Coorg Pure Arabica Coffee',
-        href: '#',
-        imageSrc: 'https://m.media-amazon.com/images/I/61RERFdp0JL._AC_UF1000,1000_QL80_.jpg',
-        imageAlt: 'Coorg Pure Arabica Coffee',
-        price: '$15',
-        color: 'Dark Roast',
-      }, {
-        id: 1,
-        name: 'Coorg Pure Arabica Coffee',
-        href: '#',
-        imageSrc: 'https://5.imimg.com/data5/SELLER/Default/2022/8/VE/CW/VD/110161676/filter-coffee-1-768x768.jpg',
-        imageAlt: 'Coorg Pure Arabica Coffee',
-        price: '$15',
-        color: 'Dark Roast',
-      }, {
-        id: 1,
-        name: 'Coorg Pure Arabica Coffee',
-        href: '#',
-        imageSrc: 'https://www.bigbasket.com/media/uploads/p/xl/40176781_3-tata-coffee-grand-filter-coffee.jpg',
-        imageAlt: 'Coorg Pure Arabica Coffee',
-        price: '$15',
-        color: 'Dark Roast',
-      }, {
-        id: 1,
-        name: 'Coorg Pure Arabica Coffee',
-        href: '#',
-        imageSrc: 'https://www.bigbasket.com/media/uploads/p/xxl/40279022_4-tata-coffee-grand-premium-coffee-powder-100-blend-instant-coffee-rich-in-aroma.jpg',
-        imageAlt: 'Coorg Pure Arabica Coffee',
-        price: '$15',
-        color: 'Dark Roast',
-      },
+      id: 2,
+      name: 'Coorg Pure Arabica Coffee',
+      href: '#',
+      imageSrc: 'https://hattikaapi.com/wp-content/uploads/2019/08/Pure-Pouch-1.png',
+      imageAlt: 'Coorg Pure Arabica Coffee',
+      price: '$15',
+      color: 'Dark Roast',
+    },
+    {
+      id: 3,
+      name: 'Coorg Pure Arabica Coffee',
+      href: '#',
+      imageSrc: 'https://hattikaapi.com/wp-content/uploads/2019/08/Chicory-Tin.png',
+      imageAlt: 'Coorg Pure Arabica Coffee',
+      price: '$15',
+      color: 'Dark Roast',
+    },
+    {
+      id: 4,
+      name: 'Coorg Pure Arabica Coffee',
+      href: '#',
+      imageSrc: 'https://www.gestures.in/cdn/shop/products/890614350059-1_1200x1200.jpg?v=1652438506',
+      imageAlt: 'Coorg Pure Arabica Coffee',
+      price: '$15',
+      color: 'Dark Roast',
+    },
+    {
+      id: 5,
+      name: 'Coorg Pure Arabica Coffee',
+      href: '#',
+      imageSrc: 'https://m.media-amazon.com/images/I/61RERFdp0JL._AC_UF1000,1000_QL80_.jpg',
+      imageAlt: 'Coorg Pure Arabica Coffee',
+      price: '$15',
+      color: 'Dark Roast',
+    },
+    {
+      id: 6,
+      name: 'Coorg Pure Arabica Coffee',
+      href: '#',
+      imageSrc: 'https://5.imimg.com/data5/SELLER/Default/2022/8/VE/CW/VD/110161676/filter-coffee-1-768x768.jpg',
+      imageAlt: 'Coorg Pure Arabica Coffee',
+      price: '$15',
+      color: 'Dark Roast',
+    },
+    {
+      id: 7,
+      name: 'Coorg Pure Arabica Coffee',
+      href: '#',
+      imageSrc: 'https://www.bigbasket.com/media/uploads/p/xl/40176781_3-tata-coffee-grand-filter-coffee.jpg',
+      imageAlt: 'Coorg Pure Arabica Coffee',
+      price: '$15',
+      color: 'Dark Roast',
+    },
+    {
+      id: 8,
+      name: 'Coorg Pure Arabica Coffee',
+      href: '#',
+      imageSrc: 'https://www.bigbasket.com/media/uploads/p/xxl/40279022_4-tata-coffee-grand-premium-coffee-powder-100-blend-instant-coffee-rich-in-aroma.jpg',
+      imageAlt: 'Coorg Pure Arabica Coffee',
+      price: '$15',
+      color: 'Dark Roast',
+    },
     // More products...
   ];
   
@@ -107,6 +113,15 @@ const products = [
                     <p className="mt-1 text-sm text-gray-500">{product.color}</p>
                   </div>
                   <p className="text-sm font-medium text-gray-900">{product.price}</p>
+                </div>
+                {/* Add to Cart Button */}
+                <div className="mt-4">
+                  <button
+                    className="w-full bg-indigo-600 text-white py-2 px-4 rounded-md hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
+                    onClick={() => alert(`Added ${product.name} to cart`)}
+                  >
+                    Purchace product 
+                  </button>
                 </div>
               </div>
             ))}
