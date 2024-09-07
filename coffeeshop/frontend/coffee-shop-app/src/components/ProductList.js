555712daa4f7ce467e47187b2bd99aa7e69b6e2d
RadhwenDarraji07 <rathwenndarraji@gmail.com>
2024-09-07 04:49:05 -0700

    dealing input field

--- coffeeshop/frontend/coffee-shop-app/src/components/ProductList.js
+++ coffeeshop/frontend/coffee-shop-app/src/components/ProductList.js
@@ -213,6 +213,15 @@ export default function ProductList() {
             className="w-full h-auto transition-transform duration-500 ease-in-out transform hover:scale-110 hover:rotate-3 animate-bounceIn"
           />
         </div>
+        <div className="relative">
+  <input 
+    type="text" 
+    placeholder="Enter your email here" 
+    className="p-3 rounded-md text-gray-900 absolute left-20 top-40" 
+  />
+</div>
+<button type="button" class="text-white bg-yellow-700 hover:bg-yellow-800 focus:ring-4 focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 me-10 mb-2 dark:bg-yellow-600 dark:hover:bg-yellow-700 focus:outline-none dark:focus:ring-yellow-800 ">Get your Card</button>
+
       </section>
 
       <style>
@@ -236,6 +245,8 @@ export default function ProductList() {
           }
         `}
       </style>
+      
     </>
+    
   )
 }
