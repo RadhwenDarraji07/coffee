85778a24fdb16103760a341d423d0c1d389c7fd2
RadhwenDarraji07 <rathwenndarraji@gmail.com>
2024-09-06 17:15:09 -0700

    testing components

---
+++ coffeeshop/frontend/coffee-shop-app/src/components/test.js
@@ -0,0 +1,12 @@
+// src/components/Test.jsx
+import React from 'react';
+
+const Test = () => {
+  return (
+    <div className="p-4">
+      <p className="text-xl font-semibold">Hello World</p>
+    </div>
+  );
+}
+
+export default Test;
