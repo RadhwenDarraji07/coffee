e251ad6da1439f4e0a24c55ad96479c9a3ba7d6c
RadhwenDarraji07 <rathwenndarraji@gmail.com>
2024-09-07 06:45:46 -0700

    stats component+

--- coffeeshop/frontend/coffee-shop-app/src/App.js
+++ coffeeshop/frontend/coffee-shop-app/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import ProductList from './components/ProductList';
-
+import Stats from './components/stats';
 
 function App() {
   return (
     <div className="App">
       <main>
         <ProductList />
+        <Stats />
       </main>
     </div>
   );
